08fa6b39a9f78a7f8268e66bbfa7f5d4cab13d24
DLAndrews <dleoandrews@outlook.com>
2015-02-24 01:34:33 +0900

    这是我的亚马逊代码练习

--- AmazonQuestion/main.cpp
+++ AmazonQuestion/main.cpp
@@ -1,15 +1,47 @@
-//
-//  main.cpp
-//  AmazonQuestion
-//
-//  Created by LUPENG on 15/2/23.
-//  Copyright (c) 2015年 LUPENG. All rights reserved.
-//
-
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <algorithm>
+using namespace std;
+int lonelyinteger(vector < int > a) {
+    int ans=0;
+    vector<int> ::iterator it;
+    for(it=a.begin();it!=a.end();it++)
+    {
+        ans^=(*it);
+    }
+    return ans;
+    
+}
+int main() {
+    int res;
+    
+    int _a_size;
+    cin >> _a_size;
+    cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
+    vector<int> _a;
+    int _a_item;
+    for(int _a_i=0; _a_i<_a_size; _a_i++) {
+        cin >> _a_item;
+        _a.push_back(_a_item);
+    }
+    
+    res = lonelyinteger(_a);
+    cout << res;
+    
     return 0;
 }
+
---
+++ AmazonQuestion/q2.cpp
@@ -0,0 +1,9 @@
+//
+//  q2.cpp
+//  AmazonQuestion
+//
+//  Created by LUPENG on 15/2/23.
+//  Copyright (c) 2015年 LUPENG. All rights reserved.
+//
+
+#include <stdio.h>
